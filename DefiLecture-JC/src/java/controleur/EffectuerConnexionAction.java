841dc37df37cb0ac63c474063b47df87994ef420
KenJoelTL <joellutumba@hotmail.com>
2017-10-20 23:17:39 -0400

    Ajout Action de Connexion

--- DefiLecture-JC/src/java/controleur/EffectuerConnexionAction.java
+++ DefiLecture-JC/src/java/controleur/EffectuerConnexionAction.java
@@ -6,14 +6,13 @@
 package controleur;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import jdbc.Config;
 import jdbc.Connexion;
+import modele.Participant;
+import modele.ParticipantDAO;
 
 /**
  *
@@ -24,61 +23,45 @@ public class EffectuerConnexionAction implements Action, RequestAware, SessionAw
     private HttpSession session;
     private HttpServletRequest request;
     private HttpServletResponse response;
+    private ParticipantDAO dao;
 
     @Override
     public String execute() {
         
         System.out.println("Entrer dans l'action de connexion");
         
-        String  identifiant = request.getParameter("identifiant"),
-                motPasse = request.getParameter("motPasse"); 
+        String identifiant = request.getParameter("identifiant"),
+               motPasse    = request.getParameter("motPasse"); 
         
         System.out.println(identifiant);
         System.out.println(motPasse);
         
        // String pilote = "com.mysql.jdbc.Driver";
-       
-        Connection cnx =null;
-        ResultSet rs = null;
-        PreparedStatement requetePreparee = null; 
-        String requete = "SELECT COURRIEL FROM participant WHERE (COURRIEL = ? or PSEUDONYME = ? ) and MOT_PASSE = ?";
+
+        Participant participant;
 
         try{
-            //Étape 1 : chargement du pilote JDBC
             Class.forName(Config.DRIVER);
-            //Étape 2 : ouverture de la connection vers la base de données
             Connexion.setUrl(Config.URL);
             Connexion.setUser(Config.DB_USER);
             Connexion.setPassword(Config.DB_PWD);
-            cnx = Connexion.getInstance();
-            //Étape 3 : execution de la requete
-            requetePreparee = cnx.prepareStatement(requete);
-            //Étape 3.1 : liaison des paramètres avec les valeurs
-            requetePreparee.setString(1, identifiant);   //met la 1er ? à la valeur du courriel dans la requete
-            requetePreparee.setString(2, identifiant);   //met la 2e  ? à la valeur du pseudonyme dans la requete
-            requetePreparee.setString(3, motPasse);      //met la 3e  ? à la valeur du motPasse dans la requete
-
-            ResultSet resultat = requetePreparee.executeQuery();
+            Connection cnx = Connexion.getInstance();
+            
+            dao = new ParticipantDAO(cnx);
+            participant = dao.findByIdentifiantMotPasse(identifiant, motPasse);
+            
             // On vérifie s'il y a un résultat    
-            if(resultat.next()){
+            if(participant!=null){
                 System.out.println("Trouver résultat dans base de donnée");
-                HttpSession session = request.getSession(true);
-                session.setAttribute("connecte", resultat.getString(1));
+                session = request.getSession(true);
+                session.setAttribute("connecte", participant.getIdParticipant());
                 request.setAttribute("vue", "pageEquipe.jsp");
-                return "/index.jsp";
             }
-            else{/*
-                HttpSession session = request.getSession(true);
-                session.setAttribute("connecte", "0");*/
+            else{
                 System.out.println("Pas trouvé de résultat dans base de donnée");
                 request.setAttribute("vue", "connexion.jsp");
-                return "/index.jsp";
             }
-        }
-        catch(SQLException e){
-            System.out.println("problème dans la connexion vers la base de données : " + e);
-            request.setAttribute("vue", "connexion.jsp");
-            return "/index.jsp";
+            return "/index.jsp";           
         }
         catch(ClassNotFoundException e){ 
             System.out.println("Erreur dans le chargement du pilote :"+ e);
@@ -86,16 +69,7 @@ public class EffectuerConnexionAction implements Action, RequestAware, SessionAw
             return "/index.jsp";
         }
         finally{
-            try {
-                if(rs != null)
-                    rs.close();
-                if(requetePreparee !=null)
-                    requetePreparee.close();
-            } catch (SQLException ex) {
-                System.out.println("problème dans la fermeture de la connexion");
-            }
             Connexion.close();
-            
         }
     }
 
