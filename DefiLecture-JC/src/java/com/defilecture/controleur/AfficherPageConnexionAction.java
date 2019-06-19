ba328f31a9d4aee745bd6ca3f42d4a4a83f03a4b
z3r0b0t <mikaelnadeau0@gmail.com>
2019-06-18 23:24:35 -0400

    Réglé mergle conflicts pour la Partie 1 de la réingénierie des actions

--- DefiLecture-JC/src/java/com/defilecture/controleur/AfficherPageConnexionAction.java
+++ DefiLecture-JC/src/java/com/defilecture/controleur/AfficherPageConnexionAction.java
@@ -1,5 +1,4 @@
 /**
-<<<<<<< HEAD
     This file is part of DefiLecture.
 
     DefiLecture is free software: you can redistribute it and/or modify
@@ -34,55 +33,4 @@ public class AfficherPageConnexionAction implements Action{
             request.setAttribute("vue", "pageConnexion.jsp");
         return "/index.jsp";
     }
-=======
- * This file is part of DefiLecture.
- *
- * <p>DefiLecture is free software: you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * <p>DefiLecture is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * <p>You should have received a copy of the GNU General Public License along with DefiLecture. If
- * not, see <http://www.gnu.org/licenses/>.
- */
-package com.defilecture.controleur;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-/** @author Charles */
-public class AfficherPageConnexionAction
-    implements Action, RequestAware, SessionAware, DataReceiver {
-
-  private HttpServletRequest request;
-  private HttpServletResponse response;
-  private HttpSession session;
-
-  @Override
-  public String execute() {
-
-    if (session.getAttribute("connecte") == null) request.setAttribute("vue", "pageConnexion.jsp");
-
-    return "/index.jsp";
-  }
-
-  @Override
-  public void setRequest(HttpServletRequest request) {
-    this.request = request;
-  }
-
-  @Override
-  public void setResponse(HttpServletResponse response) {
-    this.response = response;
-  }
-
-  @Override
-  public void setSession(HttpSession session) {
-    this.session = session;
-  }
->>>>>>> master
 }
--- DefiLecture-JC/src/java/com/defilecture/controleur/AfficherPageGestionListeComptesAction.java
+++ DefiLecture-JC/src/java/com/defilecture/controleur/AfficherPageGestionListeComptesAction.java
@@ -1,5 +1,4 @@
 /**
-<<<<<<< HEAD
     This file is part of DefiLecture.
 
     DefiLecture is free software: you can redistribute it and/or modify
@@ -78,92 +77,4 @@ public class AfficherPageGestionListeComptesAction implements Action  {
         return "/index.jsp";
     
     }
-=======
- * This file is part of DefiLecture.
- *
- * <p>DefiLecture is free software: you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * <p>DefiLecture is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * <p>You should have received a copy of the GNU General Public License along with DefiLecture. If
- * not, see <http://www.gnu.org/licenses/>.
- */
-package com.defilecture.controleur;
-
-import com.defilecture.modele.Compte;
-import com.defilecture.modele.CompteDAO;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import jdbc.Config;
-import jdbc.Connexion;
-
-/** @author Joel */
-public class AfficherPageGestionListeComptesAction
-    implements Action, RequestAware, SessionAware, DataReceiver {
-  private HttpSession session;
-  private HttpServletRequest request;
-  private HttpServletResponse response;
-
-  @Override
-  public String execute() {
-    // Exclusivement pour l'Admin et le Modérateur.
-    if (session.getAttribute("connecte") != null && session.getAttribute("role") != null)
-      try {
-        if (((int) session.getAttribute("role") == Compte.MODERATEUR)
-            || ((int) session.getAttribute("role") == Compte.ADMINISTRATEUR)) {
-
-          Class.forName(Config.DRIVER);
-          Connexion.setUrl(Config.URL);
-          Connexion.setUser(Config.DB_USER);
-          Connexion.setPassword(Config.DB_PWD);
-          Connection cnx = Connexion.getInstance();
-          CompteDAO dao = new CompteDAO(cnx);
-
-          if (dao.read((int) session.getAttribute("connecte")) != null)
-            request.setAttribute("vue", "pageGestionListeCompte.jsp");
-
-          return "/index.jsp";
-        }
-      } catch (ClassNotFoundException ex) {
-        System.out.println("Erreur dans le chargement du pilote");
-        Logger.getLogger(AfficherPageGestionListeComptesAction.class.getName())
-            .log(Level.SEVERE, null, ex);
-        request.setAttribute("vue", "accueil.jsp");
-        return "/index.jsp";
-      } catch (NullPointerException ex) {
-        System.out.println("L'utilisateur n'existe pas");
-        return "/index.jsp";
-      } catch (SQLException ex) {
-        Logger.getLogger(AfficherPageGestionListeComptesAction.class.getName())
-            .log(Level.SEVERE, null, ex);
-      } finally {
-        Connexion.close();
-      }
-    return "/index.jsp";
-  }
-
-  @Override
-  public void setRequest(HttpServletRequest request) {
-    this.request = request;
-  }
-
-  @Override
-  public void setResponse(HttpServletResponse response) {
-    this.response = response;
-  }
-
-  @Override
-  public void setSession(HttpSession session) {
-    this.session = session;
-  }
->>>>>>> master
 }
