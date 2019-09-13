5b22ce5fecc998a3bed467e45c9a8211fc5da336
z3r0b0t <mikaelnadeau0@gmail.com>
2019-09-13 13:27:56 -0400

    Retour à l'ancienne version de jsPageGestionLectures

--- DefiLecture-JC/web/script/jsPageGestionLectures.js
+++ DefiLecture-JC/web/script/jsPageGestionLectures.js
@@ -1,4 +1,5 @@
 /* 
+ <!--
     This file is part of DefiLecture.
 
     DefiLecture is free software: you can redistribute it and/or modify
@@ -13,14 +14,28 @@
 
     You should have received a copy of the GNU General Public License
     along with DefiLecture.  If not, see <http://www.gnu.org/licenses/>.
-*/
+-->
+<%-- 
+    Document   : jsPageGestionLectures
+    Created on : 2018-12-16, 20:52:35
+    Author     : Roodney Aladin
+--%>
+ */
 
-function supprimerCompteDeEquipe(idCompte){
+
+
+function supprimer(idLecture){
+
   $.ajax({
-        url: '*.do?tache=effectuerSuppressionCompteDeEquipe&idCompte='+idCompte, 
+        url: '*.do?tache=effectuerSuppressionLecture&idLecture='+idLecture, 
         cache    : false,
         complete: function () {
           $("tr#lecture-"+ idLecture).remove();
+        
         }
     });
-}
+    
+    
+
+  }
+
