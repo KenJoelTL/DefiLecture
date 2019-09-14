c71af3e7e53acff6f6d38be6a1b7ebf9932c521e
z3r0b0t <mikaelnadeau0@gmail.com>
2019-09-14 16:57:11 -0400

    Ajouter le droit aux administrateurs de faire des modifications sur les équipes

--- DefiLecture-JC/src/java/com/defilecture/controleur/EffectuerModificationEquipeAction.java
+++ DefiLecture-JC/src/java/com/defilecture/controleur/EffectuerModificationEquipeAction.java
@@ -39,7 +39,7 @@ public class EffectuerModificationEquipeAction extends Action
       action = "*.do?tache=afficherPageEquipe&idEquipe=" + request.getParameter("idEquipe");
 
       if (request.getParameter("modifier") != null) {
-        if (userIsConnected() && userIsCapitaine() && request.getParameter("nom") != null) {
+        if (userIsConnected() && (userIsCapitaine() || userIsAdmin()) && request.getParameter("nom") != null) {
           try {
             Connection cnx =
                 Connexion.startConnection(Config.DB_USER, Config.DB_PWD, Config.URL, Config.DRIVER);
