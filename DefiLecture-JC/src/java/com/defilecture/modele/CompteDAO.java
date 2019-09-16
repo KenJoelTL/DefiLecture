67ca2472aeb0e5c7214ecdf6778de681321510a6
z3r0b0t <mikaelnadeau0@gmail.com>
2019-09-15 23:58:48 -0400

    Restaurer CompteDAO countCompteByIdEquipe

--- DefiLecture-JC/src/java/com/defilecture/modele/CompteDAO.java
+++ DefiLecture-JC/src/java/com/defilecture/modele/CompteDAO.java
@@ -252,7 +252,7 @@ public class CompteDAO extends DAO<Compte> {
   }
 
   public int countCompteByIdEquipe(int idEquipe) {
-    String req = "SELECT COUNT(ID_COMPTE) AS NB_MEMBRES FROM `compte` WHERE ID_EQUIPE = ?";
+    String req = "SELECT COUNT(ID_DEMANDE_EQUIPE) FROM `demande_equipe` WHERE ID_EQUIPE = ? and STATUT_DEMANDE = 1"
     int nbMembre = 0;
     PreparedStatement paramStm = null;
 
@@ -261,7 +261,7 @@ public class CompteDAO extends DAO<Compte> {
       paramStm.setInt(1, idEquipe);
       ResultSet resultat = paramStm.executeQuery();
 
-      if (resultat.next()) nbMembre = resultat.getInt("NB_MEMBRES");
+      if (resultat.next()) nbMembre = resultat.getInt("COUNT(ID_DEMANDE_EQUIPE)");
 
       resultat.close();
       paramStm.close();
